11b39d5cf827896326933f9f9ef57aa1410f6526
Jean Tessier <jean.tessier@gusto.com>
2019-12-17 18:49:38 -0800

    Add a trivial Project model
        $ bin/rails g model Project title:string
        $ bin/rake db:migrate
        $ bin/rake db:seed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   root to: 'pages#index'
   resources :sessions, only: [:update]
+  resources :projects
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times {|i| Project.create!({title: "Project #{i + 1}"}) }
