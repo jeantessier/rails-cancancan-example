327571de094b7465af623c156eb3cf3280f77795
Jean Tessier <jean.tessier@gusto.com>
2019-12-17 20:44:47 -0800

    Remove authorization code duplication

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,50 +1,31 @@
 class ProjectsController < ApplicationController
-  def index
-    @projects = Project.all
-    authorize! :index, @project
-  end
-
-  def new
-    @project = Project.new
-    authorize! :new, @project
-  end
-
-  def create
-    @project = Project.new(project_params)
-    if @project.save
-      flash[:success] = 'Project was saved!'
-      redirect_to root_path
-    else
-      render 'new'
-    end
-    authorize! :create, @project
-  end
-
-  def edit
-    @project = Project.find(params[:id])
-    authorize! :edit, @project
-  end
+  load_and_authorize_resource
 
   def update
-    @project = Project.find(params[:id])
     if @project.update_attributes(project_params)
       flash[:success] = 'Project was updated!'
       redirect_to root_path
     else
       render 'edit'
     end
-    authorize! :update, @project
+  end
+
+  def create
+    if @project.save
+      flash[:success] = 'Project was saved!'
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
-    @project = Project.find(params[:id])
     if @project.destroy
       flash[:success] = 'Project was destroyed!'
     else
       flash[:warning] = 'Cannot destroy this project...'
     end
     redirect_to root_path
-    authorize! :destroy, @project
   end
 
   private
