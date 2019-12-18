7c9049845c4d74ad172a7a16f41f7d094edd5bb5
Jean Tessier <jean.tessier@gusto.com>
2019-12-17 20:44:08 -0800

    Add all CRUD operations for Project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,53 @@ class ProjectsController < ApplicationController
     @projects = Project.all
     authorize! :index, @project
   end
+
+  def new
+    @project = Project.new
+    authorize! :new, @project
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:success] = 'Project was saved!'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+    authorize! :create, @project
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+    authorize! :edit, @project
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      flash[:success] = 'Project was updated!'
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+    authorize! :update, @project
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    if @project.destroy
+      flash[:success] = 'Project was destroyed!'
+    else
+      flash[:warning] = 'Cannot destroy this project...'
+    end
+    redirect_to root_path
+    authorize! :destroy, @project
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title)
+  end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    alias_action :update, :destroy, :to => :modify
+
     user ||= User.new
 
     if user.role?(:admin)
       can :manage, :all
+    elsif user.role?(:moderator)
+      can [:create, :read, :update], Project
+    elsif user.role?(:user)
+      can :read, Project
     end
   end
 end
